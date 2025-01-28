6461642954c092d56056161210c4c05bf9f7e9cd
Sumanth-M-D <sumanthmdevadiga67@gmail.com>
2025-01-28 11:05:25 +0530

    fix: bug fix

--- client/src/custom_hooks/useLoadFolderData.tsx
+++ client/src/custom_hooks/useLoadFolderData.tsx
@@ -39,7 +39,7 @@ export function useLoadFolderData() {
     }
 
     getFolderData();
-  }, [dispatch, userData.rootFolder]);
+  }, [dispatch, userData?.rootFolder]);
 
   return { isFolderDataLoading };
 }
